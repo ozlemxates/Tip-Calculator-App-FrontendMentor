2093c2862cd38d6342c2c51b5990502eec718441
Özlem Ateş <ozlemxatess@outlook.com>
2023-06-07 16:32:25 +0300

    desktop design is better

---
+++ script.js
@@ -0,0 +1,30 @@
+function calculateTip() {
+    const bill = parseInt(document.getElementById("bill").value);
+    let tipPercent;
+  
+    if (document.getElementById("percent1").checked) {
+      tipPercent = 0.05;
+    } else if (document.getElementById("percent2").checked) {
+      tipPercent = 0.1;
+    } else if (document.getElementById("percent3").checked) {
+      tipPercent = 0.15;
+    } else if (document.getElementById("percent4").checked) {
+      tipPercent = 0.25;
+    } else if (document.getElementById("percent5").checked) {
+      tipPercent = 0.5;
+    } else {
+      const customPercent = parseInt(document.getElementById("percent6").value);
+      tipPercent = customPercent / 100;
+    }
+  
+    const numberOfPeople = parseInt(document.getElementById("numberOfPeople").value);
+    const tip = bill * tipPercent;
+    const tipPerPerson = tip / numberOfPeople;
+  
+    
+    document.getElementById('tipAmount').innerHTML = tip || '$0,00';
+    document.getElementById('tipPerPerson').innerHTML = tipPerPerson || '$0,00';
+  }
+
+  calculateTip()
+  
