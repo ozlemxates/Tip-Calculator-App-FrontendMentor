f9e6a455a8b25bdd963f9df15a477efffd9a6809
Özlem Ateş <ozlemxatess@outlook.com>
2023-06-12 16:20:07 +0300

    if statements romoved

--- script.js
+++ script.js
@@ -1,59 +1,60 @@
 function calculateTip() {
-    const bill = parseInt(document.getElementById("bill").value);
-    let tipPercent;
+  const bill = parseInt(document.getElementById("bill").value);
+  let tipPercent;
   
-    if (document.getElementById("percent1").checked) {
-      tipPercent = 0.05;
-    } else if (document.getElementById("percent2").checked) {
-      tipPercent = 0.1;
-    } else if (document.getElementById("percent3").checked) {
-      tipPercent = 0.15;
-    } else if (document.getElementById("percent4").checked) {
-      tipPercent = 0.25;
-    } else if (document.getElementById("percent5").checked) {
-      tipPercent = 0.5;
-    } else {
-      const customPercent = parseInt(document.getElementById("percent6").value);
-      tipPercent = customPercent / 100;
-    }
-  
-    const numberOfPeople = parseInt(document.getElementById("numberOfPeople").value);
-    const tip = bill * tipPercent;
-    const tipPerPerson = (tip+bill) / numberOfPeople;
-
-    document.getElementById('tipAmount').innerHTML = isNaN(tip) ? '$0.00' : '$' + tip.toFixed(2);
-    document.getElementById('tipPerPerson').innerHTML = isNaN(tipPerPerson) ? '$0.00' : '$' + tipPerPerson.toFixed(2);
-  }
-
-  const buttons = document.getElementsByClassName('button');
-  const input = document.getElementById('percent6');
-  let lastClickedButton = null;
-
-  const handleClick = function() {
-    if (lastClickedButton !== null) {
-      lastClickedButton.classList.remove('clicked');
-    }
-    
-    this.classList.add('clicked');
-    lastClickedButton = this;
-    
-    input.value = this.innerText;
-    input.classList.add('inputClicked');
-  };
-
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', handleClick);
-  }
-
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-      calculateTip();
-    }
+  document.getElementById("percent1").addEventListener("click", function() {
+    tipPercent = 0.05;
+  });
+  document.getElementById("percent2").addEventListener("click", function() {
+    tipPercent = 0.1;
+  });
+  document.getElementById("percent3").addEventListener("click", function() {
+    tipPercent = 0.15;
+  });
+  document.getElementById("percent4").addEventListener("click", function() {
+    tipPercent = 0.25;
+  });
+  document.getElementById("percent5").addEventListener("click", function() {
+    tipPercent = 0.5;
+  });
+  const customPercent = parseInt(document.getElementById("percent6").value);
+  document.getElementById("percent6").addEventListener("click", function() {
+    tipPercent = customPercent / 100;
   });
 
-  calculateTip();
+  console.log("which percent", tipPercent)
 
-  
-  
+  const numberOfPeople = parseInt(document.getElementById("numberOfPeople").value);
+  const tip = bill * tipPercent;
+  const tipPerPerson = (tip+bill) / numberOfPeople;
 
+  document.getElementById('tipAmount').innerHTML = isNaN(tip) ? '$0.00' : '$' + tip.toFixed(2);
+  document.getElementById('tipPerPerson').innerHTML = isNaN(tipPerPerson) ? '$0.00' : '$' + tipPerPerson.toFixed(2);
+}
+
+const buttons = document.getElementsByClassName('button');
+const input = document.getElementById('percent6');
+let lastClickedButton = null;
+
+const handleClick = function() {
+  if (lastClickedButton !== null) {
+    lastClickedButton.classList.remove('clicked');
+  }
+  
+  this.classList.add('clicked');
+  lastClickedButton = this;
+  
+  input.value = this.innerText;
+  input.classList.add('inputClicked');
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', handleClick);
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    calculateTip();
+  }
+});
   
